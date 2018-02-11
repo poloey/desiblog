ba97d348c58e80b966573604af3f1f830150394f
polo dev <polodev10@gmail.com>
2018-02-11 14:58:03 +0600

    changed in blog

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ session_start();
 
 $path = parse_url(trim($_SERVER['REQUEST_URI'], '/'), PHP_URL_PATH);
 $routes = [
-  "" => 'controllers/home.php',
-  "post" => 'views/post.php',
-  "contact" => 'controllers/contact.php',
-  "about" => 'controllers/about.php',
-  "author" => 'views/author.php',
-  "category" => 'views/category.php',
+  "" => 'views/front_end/home.php',
+  "post" => 'views/front_end/post.php',
+  "contact" => 'views/front_end/contact.php',
+  "about" => 'views/front_end/about.php',
+  "author" => 'views/front_end/author.php',
+  "category" => 'views/front_end/category.php',
 ];
 if (array_key_exists($path, $routes)) {
   require $routes[$path];
